c965ea2ba4e50dea7bf9d3c8ec35cfb0297ece7a
Frank Panzer <dev@panzerit.de>
2025-02-27 04:24:13 +0100

    Update spielfeld.php
    🔹 Was wurde geändert?
    
    Ein Copyright-Hinweis am Anfang hinzugefügt 📜
    Klarere Code-Kommentare für bessere Verständlichkeit 📝

--- spielfeld.php
+++ spielfeld.php
@@ -1,15 +1,23 @@
 <?php
-        $feld = array (
-            array(0,3,5,5,5,5,5,5,5,0),  //array für die erste Zeile (0=Ecke 3= Feld für Buchstabenrand 5=Dummy)
-            array(4,1,2,1,2,1,2,1,2,4),  //4= Zahlenrand 1= Dunkel 2= Hell
-            array(4,2,1,2,1,2,1,2,1,4),
-            array(4,1,2,1,2,1,2,1,2,4),
-            array(4,2,1,2,1,2,1,2,1,4),
-            array(4,1,2,1,2,1,2,1,2,4),
-            array(4,2,1,2,1,2,1,2,1,4),
-            array(4,1,2,1,2,1,2,1,2,4),
-            array(4,2,1,2,1,2,1,2,1,4),
-            array(0,3,5,5,5,5,5,5,5,0)
-            );
-            return $feld;
-    ?>
+/**
+ * Copyright (c) 2025 Frank Panzer | PanzerIT.de
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Script generiert das Schachbrett für das Schachspiel.
+ */
+
+$feld = array (
+    array(0,3,5,5,5,5,5,5,5,0),  // 0=Ecke, 3= Feld für Buchstabenrand, 5=Dummy
+    array(4,1,2,1,2,1,2,1,2,4),  // 4= Zahlenrand, 1= Dunkel, 2= Hell
+    array(4,2,1,2,1,2,1,2,1,4),
+    array(4,1,2,1,2,1,2,1,2,4),
+    array(4,2,1,2,1,2,1,2,1,4),
+    array(4,1,2,1,2,1,2,1,2,4),
+    array(4,2,1,2,1,2,1,2,1,4),
+    array(4,1,2,1,2,1,2,1,2,4),
+    array(4,2,1,2,1,2,1,2,1,4),
+    array(0,3,5,5,5,5,5,5,5,0)
+);
+
+return $feld;
+?>
